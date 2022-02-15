702c3b197a47910c1eca29cc31f092acc4fd7c2d
KirilPanika <kirils.jemeljanenko@inbox.lv>
2022-02-15 11:25:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // The first homework "Variables"
+
+        byte b1 = 12;                   // 12 in decimal number system
+        System.out.println(b1);
+        System.out.println("");
+
+        short s1 = 1300;
+        System.out.println(s1);
+        System.out.println("");
+
+        int i1 = 777_777;
+        System.out.println(i1);
+        System.out.println("");
+
+        long l1 = 123456789l;
+        System.out.println(l1);
+        System.out.println("");
+
+        float f1 = 3.14f;
+        float f2 = 5.78F;
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println("");
+
+        double d1 = 87.89d;
+        double d2 = 69.99D;
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println("");
+
+        boolean c1 = true;
+        boolean c2 = false;
+        System.out.println(c1);
+        System.out.println(c2);
+
+        System.out.println("");
+
+        char v1 = 'a';                  // a
+        char v2 = 65;                   // A
+        System.out.println(v1);
+        System.out.println(v2);
+    }
+}
