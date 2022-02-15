a9e5396047bdebca80af3ee7498513a601664662
KirilPanika <kirils.jemeljanenko@inbox.lv>
2022-02-15 11:55:32 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,29 @@ public class Main {
         // The first homework "Variables"
 
         byte b1 = 12;                   // 12 in decimal number system
-        System.out.println(b1);
-        System.out.println("");
-
         short s1 = 1300;
-        System.out.println(s1);
-        System.out.println("");
-
         int i1 = 777_777;
-        System.out.println(i1);
-        System.out.println("");
-
         long l1 = 123456789l;
-        System.out.println(l1);
-        System.out.println("");
-
         float f1 = 3.14f;
         float f2 = 5.78F;
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println("");
-
         double d1 = 87.89d;
         double d2 = 69.99D;
-        System.out.println(d1);
-        System.out.println(d2);
-        System.out.println("");
-
         boolean c1 = true;
         boolean c2 = false;
-        System.out.println(c1);
-        System.out.println(c2);
-
-        System.out.println("");
-
         char v1 = 'a';                  // a
         char v2 = 65;                   // A
-        System.out.println(v1);
-        System.out.println(v2);
+
+        System.out.println("byte is: " + b1);
+        System.out.println("short is: " + s1);
+        System.out.println("int is: " + i1);
+        System.out.println("long is: " + l1);
+        System.out.println("float first is: " + f1);
+        System.out.println("float second is: " + f2);
+        System.out.println("double first is " + d1);
+        System.out.println("double second is " + d2);
+        System.out.println("boolean first: " + c1);
+        System.out.println("boolean second: " + c2);
+        System.out.println("char first is " + v1);
+        System.out.println("char second is " + v2);
     }
 }
