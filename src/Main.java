795d3d0584dca71b9b2066200e7578c3bcc5fb26
KirilPanika <kirils.jemeljanenko@inbox.lv>
2022-02-15 11:29:40 +0200

    4-th

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         // The first homework "Variables"
-        // tetststststs
+
         byte b1 = 12;                   // 12 in decimal number system
         System.out.println(b1);
         System.out.println("");
